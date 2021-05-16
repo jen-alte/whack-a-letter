2c81fbd0d09c87c9ee29d97810a4d1fd4284c675
jenny-altemose <jlabiosci@gmail.com>
2021-05-16 15:42:26 -0400

    completed project

--- script.js
+++ script.js
@@ -1,24 +1,76 @@
-const typedValue = "";
 const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-const alphaIndex = []
+
 let letter = ""
-let position
+let position = ""
 const posIndex = [1, 2, 3, 4, 5]
+let score = 0;
+let roundNumber = 1;
+let gameRunning = false;
 //Increment the round number each turn and set timeOut for each round
-let round = function () {
-    for (round = 1, round <6, round++)
-    {setTimeout(function(){ alert(`{$round}}`); }, 10000*(1/round));}
+document.getElementById('start').addEventListener('click', () => {
+    //Button does nothing if the game is already running
+    if (gameRunning) {
+        return false;
+    }
+    //Change gameRunning to true
+    gameRunning = true;
+    //Clear input letters and reset score and roundNumber
+    if (position !== ""){
+        document.querySelector(position).innerHTML = "";
+        score = 0;
+        roundNumber = 1;
+    }
+    generateLetter();
+    beginRound(roundNumber);
+})
+
+document.addEventListener("keypress", (e) => {
+    //If gameRunning is false, return so key press doesn't do anything
+    if (!gameRunning) {
+        return;
+    }
+    if(e.key === letter){
+        //Increment score
+        score++;
+        document.querySelector("#scoreNum").innerHTML = score;
+        //Increment round
+        roundNumber++;
+        //Clear the letter and positioning
+        letter = "";
+        document.querySelector(position).innerHTML = "";
+        //Clear timer
+        clearTimeout(roundTimer);
+        //Generate a new letter
+        generateLetter();
+        //Reset timer and begin next round
+        beginRound(roundNumber);
+    } else {
+        //Alert "Game over!" and reset timer
+        gameOver();
+        clearTimeout(roundTimer);
+    }
+})
+
+function gameOver() {
+    gameRunning = false;
+    alert("Game over!");
 }
 
-
-document.getElementById('start').addEventListener('click', () => {
-    //Clear input letters
-    typedValue = "";
+function generateLetter(){
     //Generate a random index from 0-25
-    alphaIndex = Math.floor(Math.random() * alphabet.length-1);
+    let alphaIndex = Math.ceil(Math.random() * alphabet.length-1);
     //Generate a random letter
     letter = alphabet[alphaIndex];
     //Select a random position 1-5 and create a string of the location
-    position = "#pos" + String(Math.floor(Math.random() * posIndex.length));
-    //Place letter at position????
-
+    position = "#pos" + String(Math.ceil(Math.random() * posIndex.length));
+    //Place letter at position
+    document.querySelector(position).innerHTML = letter;
+}
+//Create a timer
+let roundTimer = null;
+function beginRound(roundNumber){
+    roundTimer = setTimeout(
+        function(){gameOver();}, 
+        10000*(1/roundNumber)
+    );
+}
